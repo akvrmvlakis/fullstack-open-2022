9cac4e45d03070bd1c881a21865e192fd075c66b
Alexandros Kourmoulakis <pro_balla2000@hotmail.com>
2022-12-11 17:38:58 +0200

    1.7 completed

---
+++ part1/1.7/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  const headers = {
+    feedbackHeader: "Give Feedback",
+    statisticsHeader: "Statistics",
+  };
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const Display = ({ counter }) =>
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + bad + neutral)}</p>
+      <p>positive {good / (good + neutral + bad) * 100} %</p>
+    </div>
+
+  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>{headers.feedbackHeader}</h1>
+      <Button
+        onClick={increaseGood}
+        text='Good'
+      />
+      <Button
+        onClick={increaseNeutral}
+        text='Neutral'
+      />
+      <Button
+        onClick={increaseBad}
+        text='Bad'
+      />
+      <h1>{headers.statisticsHeader}</h1>
+      <Display counter={Display} />
+    </div>
+
+  )
+}
+
+export default App
